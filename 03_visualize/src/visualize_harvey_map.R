4c3078d7e2ef3a8ad2a46811a429cc1ecf018318
Abner Bogan <abnerbog@buffalo.edu>
2025-09-25 13:12:41 -0400

    Add a few print statements for debugging

--- 03_visualize/src/visualize_harvey_map.R
+++ 03_visualize/src/visualize_harvey_map.R
@@ -112,9 +112,12 @@ create_map <- function(harvey_data, site_info, out_dir, out_file) {
   m <- add_gage_site_legend(m, color = usgs_gage_color, label = "USGS Gage Sites", position = "bottomright")
   
   # need to use withr::with_dir to get SaveWidget to work in Binder: https://github.com/ramnathv/htmlwidgets/issues/299#issuecomment-565754320
-  with_dir(out_dir,saveWidget(m, file = out_file, selfcontained = TRUE))
+  out_path_with_dir <- with_dir(out_dir,saveWidget(m, file = out_file, selfcontained = TRUE))
 
   out_path <- file.path(out_dir,out_file)
+
+  print(out_path_with_dir)
+  print(out_path)
   
   return(out_path)
 }
