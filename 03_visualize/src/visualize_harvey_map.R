1867a1cd6f8ff81606b5ea0a1a846a64297a91a3
Abner Bogan <abnerbog@buffalo.edu>
2025-09-25 12:54:56 -0400

    Return path of leaflet map in leaflet map target

--- 03_visualize/src/visualize_harvey_map.R
+++ 03_visualize/src/visualize_harvey_map.R
@@ -112,9 +112,9 @@ create_map <- function(harvey_data, site_info, out_dir, out_file) {
   m <- add_gage_site_legend(m, color = usgs_gage_color, label = "USGS Gage Sites", position = "bottomright")
   
   # need to use withr::with_dir to get SaveWidget to work in Binder: https://github.com/ramnathv/htmlwidgets/issues/299#issuecomment-565754320
-  with_dir(out_dir,saveWidget(m, file = out_file, selfcontained = TRUE))
+  out_path <- (out_dir,saveWidget(m, file = out_file, selfcontained = TRUE))
   
-  return(out_file)
+  return(out_path)
 }
 
 #' @title Add USGS Gage Site Legend to Leaflet Map
