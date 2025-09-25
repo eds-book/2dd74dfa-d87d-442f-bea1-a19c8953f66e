048899be4e11a49785b1fb77945cae31a50b28f8
Abner Bogan <abnerbog@buffalo.edu>
2025-09-25 12:58:39 -0400

    Fix typo

--- 03_visualize/src/visualize_harvey_map.R
+++ 03_visualize/src/visualize_harvey_map.R
@@ -112,7 +112,7 @@ create_map <- function(harvey_data, site_info, out_dir, out_file) {
   m <- add_gage_site_legend(m, color = usgs_gage_color, label = "USGS Gage Sites", position = "bottomright")
   
   # need to use withr::with_dir to get SaveWidget to work in Binder: https://github.com/ramnathv/htmlwidgets/issues/299#issuecomment-565754320
-  out_path <- (out_dir,saveWidget(m, file = out_file, selfcontained = TRUE))
+  out_path <- with_dir(out_dir,saveWidget(m, file = out_file, selfcontained = TRUE))
   
   return(out_path)
 }
