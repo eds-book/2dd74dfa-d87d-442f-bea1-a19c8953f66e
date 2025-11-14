2ca465e062f05bc14bf2f35b877aad6455a9bae1
Abner Bogan <abnerbog@buffalo.edu>
2025-11-14 12:06:10 -0500

    Improve readability of main pipeline receipe file

--- _targets.R
+++ _targets.R
@@ -1,3 +1,6 @@
+# the _targets.R file defines the entire data pipeline for the `targets` package, specifying the steps (targets), their inputs, and execution order
+# NOTE: while this file can be sourced directly to load and define the pipeline structure in your R session, the pipeline is executed and managed using `targets`` commands like `targets::tar_make()`
+
 # import targets library
 library(targets)
 
@@ -22,18 +25,18 @@ tar_option_set(
 # USGS gage sites by ID
 harvey_sites <- c('USGS-08211520','USGS-08188500','USGS-08030500','USGS-08162000','USGS-08014800')
 # start date for retrieving stage data
-start_date <- "2017-08-25"
+start_date <- '2017-08-25'
 # end date for retrieving stage data
-end_date <- "2017-09-12"
+end_date <- '2017-09-12'
 # Hurricane Harvey inputs to feed into National Hurricane Center (NHC) query
-ocean <- "al"
-storm_num <- "09"
-year <- "2017"
+ocean <- 'al'
+storm_num <- '09'
+year <- '2017'
 
-# import files for each phase receipt
-source('01_fetch.R')
-source('02_process.R')
-source('03_visualize.R')
+# import workflow phase files containing ordered list of steps to execute each workflow phase
+source('01_fetch.R')        # individual steps in fetch phase are defined (in order) in the list `p1` defined in `01_fetch.R`
+source('02_process.R')      # individual steps in process phase are defined (in order) in the list `p2` defined in `02_process.R`
+source('03_visualize.R')    # individual steps in visualize phase are defined (in order) in the list `p3` defined in `03_visualize.R`
 
-# combine all targets
-c(p1,p2,p3)
+# the final object must be a list of targets: this combines all phased steps in order
+list(p1,p2,p3)
